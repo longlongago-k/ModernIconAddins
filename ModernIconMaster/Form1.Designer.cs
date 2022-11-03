ab4f2e6f31887aa338cee61e8f6e5220714b9d3e
longlongago <longlongago.k@gmail.com>
2022-11-03 23:05:39 +0900

    スタンドアロン版にアプリアイコンを追加

--- ModernIconMaster/Form1.Designer.cs
+++ ModernIconMaster/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.iconVIewer = new ModernIconLib.UI.IconVIewer();
             this.SuspendLayout();
             // 
@@ -45,6 +46,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.iconVIewer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Modern icons";
             this.Load += new System.EventHandler(this.Form1_Load);
